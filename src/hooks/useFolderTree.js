ce9620582288d0b0bdb98211f4e03be7fbe7a6ff
Subham <63cbc9b6cd6a09abe71f8dca@bots.bitbucket.org>
2024-04-08 19:21:32 +0530

    Change url

--- src/hooks/useFolderTree.js
+++ src/hooks/useFolderTree.js
@@ -7,7 +7,10 @@ const useFolderTree = () => {
   const [folderTree, setFolderTree] = useState();
 
   const getFolderTreeServiceCall = async () => {
-    const result = await axios.post(`/rpc.FolderService/Tree`, {});
+    const result = await axios.post(
+      `http://3.134.98.180:5000/rpc.FolderService/Tree`,
+      {}
+    );
     setFolderTree(result.data);
   };
 
